d746a6a29fc889e15472d653e127ae5f4516e0ea
wiktorperczak <wiktor.prczk@gmail.com>
2024-06-15 15:32:16 +0200

    Add emails from the last 10 days option

--- src/list_messages.rs
+++ src/list_messages.rs
@@ -6,6 +6,7 @@ extern crate native_tls;
 use std::net::TcpStream;
 use native_tls::TlsStream;
 use std::io::{self, BufRead};
+use std::time::SystemTime;
 
 
 pub fn acces_to_messages(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> imap::error::Result<()> {
@@ -17,9 +18,41 @@ pub fn acces_to_messages(imap_session : &mut imap::Session<TlsStream<TcpStream>>
     let option_number : u32 = option.trim().parse().expect("Incorrect number was given");
 
     let _ = match option_number {
-        1 => unread_emails(imap_session),
+        1 => emails_last_ten_days(imap_session),
+        2 => unread_emails(imap_session),
         _ => Ok(()),
     };
+
+    Ok(())
+}
+
+fn emails_last_ten_days(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> imap::error::Result<()> {
+    // Oblicz datę sprzed 10 dni
+    let ten_days_ago = SystemTime::now() - std::time::Duration::from_secs(3600 * 1); // 86400 sekund = 1 dzień
+
+    // Formatuj datę do formatu wymaganego przez serwer IMAP (YYYY-MM-DD)
+    let date_format = "%d-%b-%Y"; // Format daty zgodny z protokołem IMAP
+    let date_str = chrono::DateTime::<chrono::Utc>::from(ten_days_ago).format(date_format).to_string();
+    
+    // Buduj zapytanie IMAP
+    let query = format!("SINCE {}", date_str);
+
+    // Wysyłamy zapytanie do serwera IMAP
+    let uids = imap_session.search(query.as_str())?;
+
+    let mut all_parsed_emails = String::new();
+    // Iterujemy po każdym UID i pobieramy odpowiednie wiadomości
+    for uid in uids.iter() {
+        let messages = imap_session.fetch(uid.to_string(), "RFC822")?;
+
+        for message in messages.iter() {
+            if let Some(body) = message.body() {
+                let parsed_content = email_parser::parse_email(body);
+                all_parsed_emails.push_str(parsed_content.as_str());
+            }
+        }
+    }
+    save_to_txt::save_to_txt(all_parsed_emails);
 
     Ok(())
 }
@@ -30,9 +63,9 @@ fn unread_emails(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> ima
     let uids = imap_session.search(query)?;
 
     // Fetch and display the messages
+    let mut all_parsed_emails = String::new();
     for uid in uids.iter() {
         let messages = imap_session.fetch(uid.to_string(), "RFC822")?;
-        let mut all_parsed_emails = String::new();
 
         for message in messages.iter() {
             if let Some(body) = message.body() {
@@ -40,8 +73,8 @@ fn unread_emails(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> ima
                 all_parsed_emails.push_str(parsed_content.as_str());
             }
         }
-        save_to_txt::save_to_txt(all_parsed_emails);
     }
+    save_to_txt::save_to_txt(all_parsed_emails);
 
     Ok(())
 }
