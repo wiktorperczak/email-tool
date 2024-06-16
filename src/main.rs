e0ed804381b3ab70699294fad141bbb4cfd87139
wiktorperczak <wiktor.prczk@gmail.com>
2024-06-16 18:17:58 +0200

    Add screen clean

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ mod list_messages;
 mod email_parser;
 mod save_to_txt;
 
+use clearscreen::clear;
+use std::process::Command;
+
 fn main() {
+    clear().expect("failed to clear screen");
     let _ = client::run();
 }
