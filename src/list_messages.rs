52652136c9b9a83530a25f9498d46dd0ce40034a
wiktorperczak <wiktor.prczk@gmail.com>
2024-06-16 12:46:46 +0200

    Add listing messages from given folder

--- src/list_messages.rs
+++ src/list_messages.rs
@@ -11,7 +11,11 @@ use std::time::SystemTime;
 
 pub fn acces_to_messages(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> imap::error::Result<()> {
     println!("Choose one of the options: ");
-    println!("1 -- list last 10 messages");
+    println!("1 -- list all messages from last 10 days");
+    println!("2 -- list all unread messages");
+    println!("3 -- list messages (from last 10 days) from the given folder");
+    println!("4 -- find all messages from given sender");
+    println!("5 -- find all messages with the given key word");
     
     let mut option = String::new();
     io::stdin().lock().read_line(&mut option).expect("Error loading data");
@@ -20,11 +24,13 @@ pub fn acces_to_messages(imap_session : &mut imap::Session<TlsStream<TcpStream>>
     let _ = match option_number {
         1 => emails_last_ten_days(imap_session),
         2 => unread_emails(imap_session),
+        3 => emails_from_folder(imap_session),
         _ => Ok(()),
     };
 
     Ok(())
 }
+
 
 fn emails_last_ten_days(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> imap::error::Result<()> {
     // 86400 seconds = 1 day
@@ -72,3 +78,49 @@ fn unread_emails(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> ima
 
     Ok(())
 }
+
+fn emails_from_folder(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> imap::error::Result<()> {
+    println!("Choose folder:");
+    println!("1 -- Starred");
+    println!("2 -- Important");
+    println!("3 -- Sent Mail");
+    println!("4 -- Drafts");
+    println!("5 -- Spam");
+    println!("6 -- Trash");
+
+    let mut option = String::new();
+    io::stdin().lock().read_line(&mut option).expect("Error loading data");
+    let option_number : u32 = option.trim().parse().expect("Incorrect number was given");
+    
+    let folder_prefix = String::from("[Gmail]/");
+    let folder_sufix = match option_number {
+        1 => String::from("Starred"),
+        2 => String::from("Important"),
+        3 => String::from("Sent Mail"),
+        4 => String::from("Drafts"),
+        5 => String::from("Spam"),
+        6 => String::from("Trash"),
+        _ => {
+            println!("Invalid option chosen.");
+            String::from("Unknown")
+        }
+    };
+    let folder = format!("{}{}", folder_prefix, folder_sufix);
+
+    imap_session.select(folder).map_err(|e| {
+        println!("Error selecting INBOX\n: {}", e);
+        e
+    })?;
+
+    let _ = emails_last_ten_days(imap_session);
+
+    Ok(())
+}
+
+// fn search_by_sender() {
+
+// }
+
+// fn search_by_key_word() {
+
+// }
