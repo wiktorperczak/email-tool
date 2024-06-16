03953cf617f79d6b8e11f64820a91096e4f000c1
wiktorperczak <wiktor.prczk@gmail.com>
2024-06-16 13:59:47 +0200

    Add listing messages from a given sender

--- src/list_messages.rs
+++ src/list_messages.rs
@@ -13,9 +13,8 @@ pub fn acces_to_messages(imap_session : &mut imap::Session<TlsStream<TcpStream>>
     println!("Choose one of the options: ");
     println!("1 -- list all messages from last 10 days");
     println!("2 -- list all unread messages");
-    println!("3 -- list messages (from last 10 days) from the given folder");
+    println!("3 -- list messages from the given folder");
     println!("4 -- find all messages from given sender");
-    println!("5 -- find all messages with the given key word");
     
     let mut option = String::new();
     io::stdin().lock().read_line(&mut option).expect("Error loading data");
@@ -25,21 +24,26 @@ pub fn acces_to_messages(imap_session : &mut imap::Session<TlsStream<TcpStream>>
         1 => emails_last_ten_days(imap_session),
         2 => unread_emails(imap_session),
         3 => emails_from_folder(imap_session),
+        4 => search_by_sender(imap_session),
         _ => Ok(()),
     };
 
     Ok(())
 }
 
-
-fn emails_last_ten_days(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> imap::error::Result<()> {
+fn days(number : u64) -> String {
     // 86400 seconds = 1 day
-    let ten_days_ago = SystemTime::now() - std::time::Duration::from_secs(86400 * 10);
+    let ten_days_ago = SystemTime::now() - std::time::Duration::from_secs(86400 * number);
 
-    let date_format = "%d-%b-%Y"; // Format daty zgodny z protokołem IMAP
+    let date_format = "%d-%b-%Y";
     let date_str = chrono::DateTime::<chrono::Utc>::from(ten_days_ago).format(date_format).to_string();
 
-    let query = format!("SINCE {}", date_str);
+    date_str
+}
+
+
+fn emails_last_ten_days(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> imap::error::Result<()> {
+    let query = format!("SINCE {}", days(10));
     let uids = imap_session.search(query.as_str())?;
 
     let mut all_parsed_emails = String::new();
@@ -60,7 +64,7 @@ fn emails_last_ten_days(imap_session : &mut imap::Session<TlsStream<TcpStream>>)
 
 
 fn unread_emails(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> imap::error::Result<()> {
-    let query = "NEW";
+    let query = format!("SINCE {}", days(10));
     let uids = imap_session.search(query)?;
 
     let mut all_parsed_emails = String::new();
@@ -117,10 +121,67 @@ fn emails_from_folder(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -
     Ok(())
 }
 
-// fn search_by_sender() {
 
-// }
+fn search_by_sender(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> imap::error::Result<()> {
+    println!("Enter email address of a sender: ");
+    let mut sender_email = String::new();
+    io::stdin().lock().read_line(&mut sender_email).expect("Error loading data");
+    let sender_email = sender_email.as_str().trim_end();
 
-// fn search_by_key_word() {
+    let query = format!("SINCE {}", days(100));
+    let uids = imap_session.search(query.as_str())?;
 
-// }
+    let mut all_parsed_emails = String::new();
+    let mut i : i32 = 0;
+
+    for uid in uids.iter() {
+        let messages = imap_session.fetch(uid.to_string(), "RFC822.HEADER")?;
+
+        for message in messages.iter() {
+            i += 1;
+            if i % 100 == 0 {
+                println!("{}", i);
+            }
+    
+            // Pobierz nagłówek "From"
+            if let Some(header) = message.header() {
+                let header_str = std::str::from_utf8(header).unwrap();              
+                if sender_email == get_sender(header_str) {
+                    println!("OK");
+
+                    let full_message = imap_session.fetch(uid.to_string(), "RFC822")?;
+                    for msg in full_message.iter() {
+                        if let Some(body) = msg.body() {
+                            let parsed_content = email_parser::parse_email(body);
+                            all_parsed_emails.push_str(parsed_content.as_str());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    save_to_txt::save_to_txt(all_parsed_emails);
+    Ok(())
+}
+
+fn get_sender(header_data: &str) -> String {
+    for line in header_data.lines() {
+        if line.to_lowercase().starts_with("from:") {
+            if let Some(start) = line.find('<') {
+                if let Some(end) = line.find('>') {
+                    return line[start + 1..end].to_string();
+                }
+            } else {
+                // If there are no angle brackets, try to extract the email directly
+                let parts: Vec<&str> = line.split_whitespace().collect();
+                for part in parts {
+                    if part.contains('@') {
+                        return part.to_string();
+                    }
+                }
+            }
+        }
+    }
+    "Unknown".to_string()
+}
