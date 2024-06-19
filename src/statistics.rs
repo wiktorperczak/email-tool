f2390732edacd8b04d899d9103eb681a0a06e70c
wiktorperczak <wiktor.prczk@gmail.com>
2024-06-19 02:16:49 +0200

    Add top senders statistics

--- src/statistics.rs
+++ src/statistics.rs
@@ -1,9 +1,12 @@
+use crate::util;
+
 extern crate imap;
 extern crate native_tls;
 
 use std::net::TcpStream;
 use native_tls::TlsStream;
 use chrono::{Datelike, Utc};
+use std::collections::HashMap;
 
 pub fn generate_statistics(mut imap_session : imap::Session<TlsStream<TcpStream>>) -> imap::Session<TlsStream<TcpStream>> {
     let line = "_".repeat(50);
@@ -11,6 +14,7 @@ pub fn generate_statistics(mut imap_session : imap::Session<TlsStream<TcpStream>
 
     let _ = folder_statistics(&mut imap_session);
     let _ = stats_by_year(&mut imap_session);
+    let _ = stats_sender(&mut imap_session);
     
     println!("\n{}\n", line);
     
@@ -86,6 +90,36 @@ fn parse_year_from_date(header_data: &str) -> Option<u32> {
         }
     }
     None
+}
+
+fn stats_sender(imap_session : &mut imap::Session<TlsStream<TcpStream>>) -> imap::error::Result<()> {
+    let mut email_count_map: HashMap<String, usize> = HashMap::new();
+    
+    imap_session.select("INBOX")?;
+    let query = format!("SINCE {}", util::days(15));
+    let uids = imap_session.search(query.as_str())?;
+
+    for uid in uids.iter() {
+        let messages = imap_session.fetch(uid.to_string(), "RFC822.HEADER")?;
+
+        for message in messages.iter() {
+            if let Some(header) = message.header() {
+                let header_str = util::get_sender(std::str::from_utf8(header).unwrap());
+                *email_count_map.entry(header_str).or_insert(0) += 1;
+            }
+        }
+    }
+
+    let mut pairs: Vec<_> = email_count_map.into_iter().collect();
+
+    pairs.sort_by_key(|&(_, v)| std::cmp::Reverse(v));
+
+    println!("\nMost messages sent (last 30 days): ");
+    for (i, (key, value)) in pairs.iter().take(10).enumerate() {
+        println!("{}. {}: {}", i + 1, key, 2 * value);
+    }
+
+    Ok(())
 }
 
 
